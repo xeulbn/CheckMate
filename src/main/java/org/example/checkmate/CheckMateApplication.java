9f383b2f67a2555c253fb783decbe52e3025954b
xeulbn <gsbtiger0215@gmail.com>
2025-07-01 00:21:36 +0900

    REBUILD

--- src/main/java/org/example/checkmate/CheckMateApplication.java
+++ src/main/java/org/example/checkmate/CheckMateApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CheckMateApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(CheckMateApplication.class, args);
     }
 
